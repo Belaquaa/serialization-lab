ed2bc63039a6c35229e2b4e57fa6d9dab01c86ba
belaqua <eee_pokkk@mail.ru>
2025-03-24 12:57:11 +0300

    codec changed: Base95 -> Base128

--- src/main/java/belaquaa/serial/serializer/BaseCodec.java
+++ src/main/java/belaquaa/serial/serializer/BaseCodec.java
@@ -2,20 +2,22 @@ package belaquaa.serial.serializer;
 
 import java.math.BigInteger;
 
+// Используем все 128 символов аськи, здесь внимательно, т.к. используются и управляющие символы
+// Если хотим на 100% избежать проблем с отображением, можно перейти на Base95, так будет хуже на 6-7%
 public class BaseCodec {
     private static char digitToChar(int digit) {
-        return (char) (digit + 32);
+        return (char) digit;
     }
 
     private static int charToDigit(char c) {
-        return c - 32;
+        return (int) c;
     }
 
     public static String encode(byte[] data) {
         BigInteger bi = new BigInteger(1, data);
-        int expectedDigits = (int) Math.ceil(data.length * Math.log(256) / Math.log(95));
+        int expectedDigits = (int) Math.ceil(data.length * Math.log(256) / Math.log(128));
         StringBuilder sb = new StringBuilder(expectedDigits);
-        BigInteger base = BigInteger.valueOf(95);
+        BigInteger base = BigInteger.valueOf(128);
         while (bi.compareTo(BigInteger.ZERO) > 0) {
             BigInteger[] divRem = bi.divideAndRemainder(base);
             sb.append(digitToChar(divRem[1].intValue()));
@@ -29,7 +31,7 @@ public class BaseCodec {
 
     public static byte[] decode(String s, int numBytes) {
         BigInteger bi = BigInteger.ZERO;
-        BigInteger base = BigInteger.valueOf(95);
+        BigInteger base = BigInteger.valueOf(128);
         for (int i = 0; i < s.length(); i++) {
             bi = bi.multiply(base).add(BigInteger.valueOf(charToDigit(s.charAt(i))));
         }
